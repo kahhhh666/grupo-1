3f908b4707545dc3cf124ed641c76c1459828893
projeto <megaplusms2@gmail.com>
2026-04-15 18:40:48 -0300

    Criação da função principal/ Pegando valores do html

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+function adicionarVeiculo(){
+    const form = document.querySelector("#formVeiculo");
+    const lista = document.querySelector("#listaVeiculos");
+
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    // pegando valores do html
+    const modelo = document.getElementById("modelo").value;
+    const marca = document.getElementById("marca").value;
+    const placa = document.getElementById("placa").value;
+}

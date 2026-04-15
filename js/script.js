720f06158c40818c07dc91d4f7f5c0bf204ee38b
projeto <megaplusms2@gmail.com>
2026-04-15 18:42:34 -0300

    pegando data (em type date)/ pegando combustivel

--- js/script.js
+++ js/script.js
@@ -28,4 +28,15 @@ function adicionarVeiculo(){
     const modelo = document.getElementById("modelo").value;
     const marca = document.getElementById("marca").value;
     const placa = document.getElementById("placa").value;
+
+    // data (type="date") coleta do dado
+    const data = document.getElementById("ano").value;
+    if (!data) return;
+
+    const ano = new Date(data).getFullYear();
+
+    const valor = parseFloat(document.getElementById("valor").value);
+    const cor = document.getElementById("cor").value;
+
+    const combustivel = document.querySelector('input[name="combustivel"]:checked')?.value;
 }
